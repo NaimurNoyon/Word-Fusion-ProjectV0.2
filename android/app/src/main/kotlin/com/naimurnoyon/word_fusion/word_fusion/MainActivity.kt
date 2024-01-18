46cf8f6c991530b3cde909246838d7c7231d2d10
Naimur Noyon <naimurrahmannoyon@gmail.com>
2024-01-18 16:44:24 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/naimurnoyon/word_fusion/word_fusion/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.naimurnoyon.word_fusion.word_fusion
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,113 @@
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:lottie/lottie.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Word Fusion',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool isDark = false;
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness: Brightness.light,
+      ),
+    );
+    return Scaffold(
+      body: Stack(
+        children: [
+          SizedBox(
+            height: double.infinity,
+            child:isDark ? Lottie.asset(
+                'assets/lottie/background_night.json',
+                fit: BoxFit.cover
+            ) : Lottie.asset(
+                'assets/lottie/background_day.json',
+                fit: BoxFit.cover
+            ),
+          ),
+          Positioned(
+            top: 40,
+            right: 16,
+            child: Switch(
+              activeColor: Colors.white,
+              activeTrackColor: Colors.black,
+              inactiveThumbColor: Colors.black,
+              inactiveTrackColor: Colors.white,
+              value: isDark,
+              onChanged: (value) {
+                setState(() {
+                  isDark = value;
+                });
+              },
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 150,
+                  width: 250,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color:isDark? Colors.white.withOpacity(0.5) : Colors.black.withOpacity(0.5),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Current Rank", style: TextStyle(color: isDark? Colors.black : Colors.white),),
+                      Text("1",style: TextStyle(fontSize: 50, color: isDark? Colors.black : Colors.white),)
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 300)
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
